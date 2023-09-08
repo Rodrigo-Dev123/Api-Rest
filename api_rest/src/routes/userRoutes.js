fb6769a32a2d691073883c6a971a10febbd2cf35
Rodrigo <rodrigojsg9@gmail.com>
2023-09-08 19:01:43 -0300

    adding new features

--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
+// Na vida real estes tipos de rotas não exitem, é só um exemplo.
+// router.get('/', loginRequired, userController.index);
+// router.get('/:id', userController.show);
+
 router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
 
